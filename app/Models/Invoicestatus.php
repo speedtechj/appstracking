dd8248c5e97e5014fcef11787f6230449adc911b
speedtechj <arbiasjr@gmail.com>
2024-04-08 13:16:55 -0600

    update

--- app/Models/Invoicestatus.php
+++ app/Models/Invoicestatus.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Invoicestatus extends Model
 {
     use HasFactory;
-    protected $table = 'invoicetatuses';
+    protected $table = 'invoicestatuses';
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 
 
 use App\Models\Invoicestatus;
-use App\Models\Invoicestatus;
 use App\Models\Sender;
 use App\Models\Booking;
 use Illuminate\Http\Request;
@@ -15,15 +14,17 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::get('/trackinvoice/{invoice}', function (string $invoice) {
-    $datas = Booking::where('booking_invoice', $invoice)
-    ->orWhere('manual_invoice',  $invoice)
-    ->get()->first();
-    $datastatus = $datas = Invoicestatus::where('generated_invoice', $invoice)
-    ->orWhere('manual_invoice',  $invoice)
-    ->get()->first();
+    // $datas = Booking::where('booking_invoice', $invoice)
+    // ->orWhere('manual_invoice',  $invoice)
+    // ->get()->first();
+    // $datastatus = $datas = Invoicestatus::where('generated_invoice', $invoice)
+    // ->orWhere('manual_invoice',  $invoice)
+    // ->get();
+
+
     // $data['sender'] = Sender::where('id', $datas->sender_id)->get()->first();
     // $data['invoicestatus'] = Invoicestatus::where('')
     // dd($datas->sender_id);
-    return $datastatus;
+    // return $datastatus;
 
 });
