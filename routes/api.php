595e1a923da9df53835877c17dc7ab16b817f925
speedtechj <arbiasjr@gmail.com>
2024-04-08 11:43:56 -0600

    update

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 
 
+use App\Models\Invoicestatus;
+use App\Models\Invoicestatus;
 use App\Models\Sender;
 use App\Models\Booking;
 use Illuminate\Http\Request;
@@ -16,8 +18,12 @@ Route::get('/trackinvoice/{invoice}', function (string $invoice) {
     $datas = Booking::where('booking_invoice', $invoice)
     ->orWhere('manual_invoice',  $invoice)
     ->get()->first();
-    $data['sender'] = Sender::where('id', $datas->sender_id)->get()->first();
+    $datastatus = $datas = Invoicestatus::where('generated_invoice', $invoice)
+    ->orWhere('manual_invoice',  $invoice)
+    ->get()->first();
+    // $data['sender'] = Sender::where('id', $datas->sender_id)->get()->first();
+    // $data['invoicestatus'] = Invoicestatus::where('')
     // dd($datas->sender_id);
-    return $data;
+    return $datastatus;
 
 });
